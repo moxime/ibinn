684608027daffd3c0cbe0292038c9fd2c1f40852
moxime <mossonce@hotmail.com>
2023-07-09 23:06:48 +0200

    oodsets : svhn

--- evaluation/ood.py
+++ evaluation/ood.py
@@ -1,3 +1,5 @@
+from model import GenerativeClassifier
+import matplotlib.pyplot as plt
 import os
 
 import torch
@@ -5,13 +7,12 @@ from tqdm import tqdm
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
-from model import GenerativeClassifier
 
 
 def outlier_detection(inn_model, data, args, test_set=False):
     ''' the option `test_set` controls, whether the test set, or the validation set is used.'''
+
+    oodsets = ags['evaluation']['oodsets'].split()
 
     import ood_datasets.imagenet
     import ood_datasets.cifar
@@ -55,81 +56,83 @@ def outlier_detection(inn_model, data, args, test_set=False):
                                      np.linspace(0.1, 0.9, num=quantile_resolution, endpoint=False),
                                      np.linspace(0.9, 1.0, num=quantile_resolution+1, endpoint=True)])
 
-
     scores_all = {}
     entrop_all = {}
     generators = []
 
     in_distrib_data = (data.test_loader if test_set else [(data.val_x, torch.argmax(data.val_y, dim=1))])
 
-    generators = [
-                      #(data.test_loader, 'test'),
-                      (ood_datasets.cifar.cifar_rgb_rotation(inn_model.args, 0.35), 'rot_rgb'),
-                      (ood_datasets.quickdraw.quickdraw_colored(inn_model.args), 'quickdraw'),
-                      (ood_datasets.cifar.cifar_noise(inn_model.args, 0.015), 'noisy'),
-                      (ood_datasets.imagenet.imagenet(inn_model.args), 'imagenet'),
-                      #(ood_datasets.svhn.svhn(inn_model.args), 'SVHN'),
-                      ]
+    # generators = [
+    #                   #(data.test_loader, 'test'),
+    #                   (ood_datasets.cifar.cifar_rgb_rotation(inn_model.args, 0.35), 'rot_rgb'),
+    #                   (ood_datasets.quickdraw.quickdraw_colored(inn_model.args), 'quickdraw'),
+    #                   (ood_datasets.cifar.cifar_noise(inn_model.args, 0.015), 'noisy'),
+    #                   (ood_datasets.imagenet.imagenet(inn_model.args), 'imagenet'),
+    #                   #(ood_datasets.svhn.svhn(inn_model.args), 'SVHN'),
+    #                   ]
 
-    for gen,label in generators:
+    if 'svhn' in oodsets:
+        generators.append(ood_datasets.svhn.svhn(inn_model.args), 'SVHN'))
+
+    for gen, label in generators:
         print(f'>> Computing OoD score for {label}')
-        scores_all[label], ent = collect_scores(gen)
-        entrop_all[label] = np.mean(ent)
+        scores_all[label], ent=collect_scores(gen)
+        entrop_all[label]=np.mean(ent)
 
-    scores_ID, entrop_ID = collect_scores(in_distrib_data, oversample=int(args['evaluation']['train_set_oversampling']))
-    entrop_ID = np.mean(entrop_ID)
-    quantiles_ID = np.quantile(scores_ID, quantile_steps)
-    typical_ID = np.mean(scores_ID)
-    typicality_scores_ID = np.abs(scores_ID - typical_ID)
+    scores_ID, entrop_ID=collect_scores(in_distrib_data, oversample = int(args['evaluation']['train_set_oversampling']))
+    entrop_ID=np.mean(entrop_ID)
+    quantiles_ID=np.quantile(scores_ID, quantile_steps)
+    typical_ID=np.mean(scores_ID)
+    typicality_scores_ID=np.abs(scores_ID - typical_ID)
 
-    fig_roc = plt.figure(figsize=(8,8))
-    plt.plot([0,1], [0,1], '--', color='gray', label='random')
+    fig_roc=plt.figure(figsize = (8, 8))
+    plt.plot([0, 1], [0, 1], '--', color = 'gray', label = 'random')
     plt.grid(True)
 
-    fig_hist = plt.figure(figsize=(8,6))
-    plt.hist(scores_ID, bins=50, histtype='step', density=True, color='gray', label='orig. distrib.')
+    fig_hist=plt.figure(figsize = (8, 6))
+    plt.hist(scores_ID, bins = 50, histtype = 'step', density = True, color = 'gray', label = 'orig. distrib.')
 
-    def auc(test_scores, train_scores, label=''):
-        xjoint = -np.sort(-np.concatenate((train_scores, test_scores)))
+    def auc(test_scores, train_scores, label = ''):
+        xjoint=-np.sort(-np.concatenate((train_scores, test_scores)))
         xjoint[-1] -= 0.0001
-        val_range = (np.min(xjoint), np.max(xjoint))
+        val_range=(np.min(xjoint), np.max(xjoint))
 
-        roc = []
+        roc=[]
         for x in xjoint:
-            fpr = np.mean(train_scores > x)
-            tpr = np.mean(test_scores > x)
+            fpr=np.mean(train_scores > x)
+            tpr=np.mean(test_scores > x)
             roc.append((fpr, tpr))
-        roc = np.array(roc).T
+        roc=np.array(roc).T
 
-        auc = np.trapz(roc[1], x=roc[0])
+        auc=np.trapz(roc[1], x = roc[0])
         plt.figure(fig_hist.number)
-        plt.hist(test_scores, bins=50, histtype='step', density=True, label=label + ' (%.4f AUC)' % (auc))
+        plt.hist(test_scores, bins = 50, histtype = 'step', density = True, label = label + ' (%.4f AUC)' % (auc))
         plt.figure(fig_roc.number)
-        plt.plot(roc[0], roc[1], label=label + ' (%.4f AUC)' % (auc))
+        plt.plot(roc[0], roc[1], label = label + ' (%.4f AUC)' % (auc))
 
         return auc
 
     def auc_quantiles(test_scores, train_quantiles, quantile_steps):
 
-        roc = []
+        roc=[]
         for i in range(len(quantile_steps) // 2):
-            fpr = (quantile_steps[i] + 1. - quantile_steps[-(1+i)])
-            tpr = np.mean(np.logical_or(test_scores < train_quantiles[i], test_scores > train_quantiles[-(i+1)]))
+            fpr=(quantile_steps[i] + 1. - quantile_steps[-(1+i)])
+            tpr=np.mean(np.logical_or(test_scores < train_quantiles[i], test_scores > train_quantiles[-(i+1)]))
             roc.append((fpr, tpr))
-        roc.append((1.,1.))
+        roc.append((1., 1.))
 
-        roc = np.array(roc).T
-        return np.trapz(roc[1], x=roc[0])
+        roc=np.array(roc).T
+        return np.trapz(roc[1], x = roc[0])
 
-    aucs_one_tailed = {}
-    aucs_two_tailed = {}
-    aucs_typicality = {}
-    delta_entropy = {}
+    aucs_one_tailed={}
+    aucs_two_tailed={}
+    aucs_typicality={}
+    delta_entropy={}
     for label, score in scores_all.items():
-        aucs_one_tailed[label] = auc(score, scores_ID, label)
-        aucs_two_tailed[label] = auc_quantiles(score, quantiles_ID, quantile_steps)
-        aucs_typicality[label] = auc(np.abs(score - typical_ID), typicality_scores_ID)
-        delta_entropy[label] = entrop_all[label] - entrop_ID
+        aucs_one_tailed[label]=auc(score, scores_ID, label)
+        aucs_two_tailed[label]=auc_quantiles(score, quantiles_ID, quantile_steps)
+        aucs_typicality[label]=auc(np.abs(score - typical_ID), typicality_scores_ID)
+        delta_entropy[label]=entrop_all[label] - entrop_ID
 
     plt.figure(fig_hist.number)
     plt.legend()
@@ -137,4 +140,3 @@ def outlier_detection(inn_model, data, args, test_set=False):
     plt.legend()
 
     return aucs_one_tailed, aucs_two_tailed, aucs_typicality, entrop_all, delta_entropy
-
