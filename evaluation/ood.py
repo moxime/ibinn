7ff495127d03b9c0611ffc67d093baab3658b4e4
moxime <mossonce@hotmail.com>
2023-09-10 17:24:47 +0200

    2T TPR

--- evaluation/ood.py
+++ evaluation/ood.py
@@ -129,9 +129,10 @@ def outlier_detection(inn_model, data, args, test_set=False, target_tpr=0.95):
         target_fpr = None
 
         for i in range(len(quantile_steps) // 2):
+            i_ = len(quantile_steps) // 2 - i - 1
             tpr = (quantile_steps[-(1 + i)] - quantile_steps[i])
             fpr = np.mean(np.logical_and(test_scores >= train_quantiles[i], test_scores <= train_quantiles[-(i + 1)]))
-            if tpr <= target_tpr and target_fpr is None:
+            if tpr > target_tpr and target_fpr is None:
                 target_fpr = fpr
                 print('{:.1%}: {:.1%}'.format(tpr, fpr))
 
